c5e81575785b15f2a4fc380ba368810b0c69c0b2
Mihai Barbu <xwave21@gmail.com>
2015-08-17 17:06:31 +0300

    Adaugare background in spate la video

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -9,20 +9,26 @@
 	<h4 class="video-page">Descoperă cele mai cool funcţii ale imprimantelor.</h4>
 
 	<h4 class="video-page">NFC în viaţa reală</h4>
-	<iframe width="708" height="411" src="//www.youtube.com/embed/-_C7mmFFTeM" frameborder="0" allowfullscreen></iframe>
+	<div class="video">
+		<iframe class="frame" width="708" height="411" src="//www.youtube.com/embed/-_C7mmFFTeM" frameborder="0" allowfullscreen></iframe>
+	</div>
 
 	<h4 class="video-page">Cu capul în nori şi cloud printing</h4>
-	<iframe width="708" height="411" src="//www.youtube.com/embed/-_C7mmFFTeM" frameborder="0" allowfullscreen></iframe>
+	<div class="video">
+		<iframe class="frame" width="708" height="411" src="//www.youtube.com/embed/-_C7mmFFTeM" frameborder="0" allowfullscreen></iframe>
+	</div>
 
 	<h4 class="video-page">Imprimă de oriunde cu Wi-fi</h4>
-	<iframe width="708" height="411" src="//www.youtube.com/embed/-_C7mmFFTeM" frameborder="0" allowfullscreen></iframe>
+	<div class="video">
+		<iframe class="frame" width="708" height="411" src="//www.youtube.com/embed/-_C7mmFFTeM" frameborder="0" allowfullscreen></iframe>
+	</div>
 </div>
 <br /><br />
 <div class="conditii">
 	<p class="conditii">Promoţie valabilă în perioada 24 august - 30 septembrie 2015.</p>
 
 	<p class="conditii">**Canon Pixma MG5650, MG6650, MG7550, MX535, MX925, LBP6030, MF3010, MF211</p>
-
+	<br />
 	<a href="#">
 		<p class="regulament">REGULAMENT ></p>
 	</a>
