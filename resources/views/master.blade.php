59272aae0c8f0358901cbd903d6c15a10af88b6e
Mihai Barbu <xwave21@gmail.com>
2015-09-01 10:53:22 +0300

    Add GA

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,31 +6,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <title>@yield('title')</title>
-        
+
         <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/bootstrap-theme.min.css') }}">
-        
+
         <link rel="stylesheet" type="text/css" href="/css/main.css">
     </head>
     <body>
         @yield('header')
 
         @yield('content')
-        
+
         <!-- @include('footer') -->
 
-        
+
         <script type="text/javascript" src="/js/lib/lodash.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
         <script src="/js/plugins.js"></script>
         <script src="/js/main.js"></script>
-        
+
         @yield('js')
 
         <script type="text/javascript">
           var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-55513824-6']);
+          _gaq.push(['_setAccount', 'UA-55513824-8']);
           _gaq.push(['_trackPageview']);
           (function() {
             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
